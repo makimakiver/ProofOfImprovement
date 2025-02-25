90bc1e130c2af92fd5a569c23547b9769adc74ca
ネルフ受付係員(29) <125832648+makimakiver@users.noreply.github.com>
2025-02-25 07:17:51 +0000

    Deployment scripts have been updated

---
+++ scaffold-move-main/packages/move/move.config.js
@@ -0,0 +1,19 @@
+
+const defaultNetwork = 'movement_testnet';
+// const overrideMoveVersion = 'move-1'; // Uncomment to use Move-1 for all networks
+
+const networks = {
+  movement_testnet: {
+    "rest-url": "https://aptos.testnet.bardock.movementlabs.xyz/v1",
+    // "faucet-url": "https://faucet.testnet.bardock.movementnetwork.xyz/",
+  },
+  movement_mainnet: {
+    "rest-url": "https://mainnet.movementnetwork.xyz/v1"
+  },
+};
+
+// Set to false if external modules should not be loaded via script.
+// If set to true, it will load modules from all addresses declared in move.toml.
+const loadExternalModules = true;
+
+module.exports = { defaultNetwork, networks, loadExternalModules };
